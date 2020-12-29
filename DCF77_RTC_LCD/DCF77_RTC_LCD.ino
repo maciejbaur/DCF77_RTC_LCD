b5f2bf0303aab3fc75b55c05746e070bae949b2e
maciejbaur <maciej.baur@gmail.com>
2020-12-29 13:05:59 +0100

    Update DCF77_RTC_LCD.ino
    Sprzątanie kodu i dodanie ciszy nocnej od 22 do 8 (brak podświetlenia i sygnału pełnej godziny).

--- DCF77_RTC_LCD/DCF77_RTC_LCD.ino
+++ DCF77_RTC_LCD/DCF77_RTC_LCD.ino
@@ -15,6 +15,9 @@ uint8_t curSec;
 DS3231 clock;
 RTCDateTime tm;
 
+const byte dzienStart = 8;      // poczatek dnia
+const byte dzienEnd = 22;       // koniec dnia
+
 const byte buzzer = 9;          // buzzer to arduino pin 9
 const int buzzerDelay1 = 100;   // przerwa w pikaniu
 const int buzzerDelay2 = 600;   // przerwa miedzy sekwencja pikania
@@ -48,13 +51,14 @@ void loop() {
   } else if (tm.day > 0) {
     digitalWrite(LED_BUILTIN, HIGH);
     if (tm.second != curSec)  {
-      rtcLCD();           // Wyswietlanie danych RTC na LCD
-      podswietlenie();    // podswietlenie wylaczane w nocy
-      RadioStopChime();          // pikanie na pelna godzine
+      rtcLCD();                 // Wyswietlanie danych RTC na LCD
+      podswietlenie();          // podswietlenie wylaczane w nocy
+      RadioStopChime();         // pikanie na pelna godzine
     }
     curSec = dt.sec;
   }
 
+  // Ustawianie RTC po odebraniu sygnalu DCF77
   if (syncOK == false && dt.day > 0)  {
     lcd.clear();
     lcd.backlight();
@@ -72,8 +76,8 @@ void loop() {
 
 // -- Funkcje pomocnicze --
 
-// Uzupelnianie do liczb dwucyfrowych na potrzeby wyswietlacza
 void print2digits(int number) {
+// Uzupelnianie do liczb dwucyfrowych na potrzeby wyswietlacza
   if (number >= 0 && number < 10) {
     lcd.write('0');
   }
@@ -81,15 +85,13 @@ void print2digits(int number) {
 }
 
 void rtcLCD ()  {
-  //tm = clock.getDateTime();
-
+  // Wyswietlanie daty, godziny i temperatury na LCD
   lcd.setCursor(0, 0); // Ustawienie kursora w pozycji 0,0 (pierwszy wiersz, pierwsza kolumna)
   print2digits(tm.hour);
   lcd.write(':');
   print2digits(tm.minute);
   lcd.write(':');
   print2digits(tm.second);
-  //lcd.print(" ");
 
   clock.forceConversion();
   lcd.setCursor(9, 0); //Ustawienie kursora w pozycji 0,12 (drugi wiersz, 12 kolumna)
@@ -98,40 +100,42 @@ void rtcLCD ()  {
   lcd.print("C");
 
   lcd.setCursor(3, 1); //Ustawienie kursora w pozycji 0,1 (drugi wiersz, pierwsza kolumna)
-  //lcd.print("   ");
   print2digits(tm.day);
   lcd.write('.');
   print2digits(tm.month);
   lcd.write('.');
   lcd.print(tm.year);
-  //lcd.print("   ");
-
 }
 
 void podswietlenie()  {
-  if (tm.hour >= 8 && tm.hour <= 22) {    // wylaczenie podswietlenia od 23 do 8
+  // Wylaczanie podswietlenia w nocy
+  if (tm.hour >= dzienStart && tm.hour <= dzienEnd) {    // wylaczenie podswietlenia od 23 do 8
     lcd.backlight();                      // zalaczenie podwietlenia
   } else {
     lcd.noBacklight();                    // wylaczenie podwietlenia
   }
 }
 
+
 void RadioStopChime() {
-  if (tm.minute == 0 && piknelo == false) { // jesli minuta jest rowna 0 i jeszcze nie piknelo
-    for (int i = 0; i < 3; i++) {           // odtwarzaj trzy sygnaly RadioStop o pelnej godzinie
-      tone(buzzer, 1160);
-      delay(buzzerDelay1);
-      tone(buzzer, 1400);
-      delay(buzzerDelay1);
-      tone(buzzer, 1670);
-      delay(buzzerDelay1);
-      noTone(buzzer);
-      delay(buzzerDelay2);
+  // Sygnal o pelnej godzinie
+  if (tm.hour >= dzienStart && tm.hour <= dzienEnd) {    // cisza nocna
+    if (tm.minute == 0 && piknelo == false) { // jesli minuta jest rowna 0 i jeszcze nie piknelo
+      for (int i = 0; i < 3; i++) {           // odtwarzaj trzy sygnaly RadioStop o pelnej godzinie
+        tone(buzzer, 1160);
+        delay(buzzerDelay1);
+        tone(buzzer, 1400);
+        delay(buzzerDelay1);
+        tone(buzzer, 1670);
+        delay(buzzerDelay1);
+        noTone(buzzer);
+        delay(buzzerDelay2);
+      }
+      piknelo = true;                         // flaga, ze piknelo
     }
-    piknelo = true;                         // flaga, ze piknelo
-  }
   
-  if (tm.minute > 0) {                      // jesli minuta jest wieksza od 0
-    piknelo = false;                        // flaga, ze nie piknelo
+    if (tm.minute > 0) {                      // jesli minuta jest wieksza od 0
+      piknelo = false;                        // flaga, ze nie piknelo
+    }
   }
 }
